a8b3be9b4d0e1be6bfcc431a635cd3c6829adc6f
Ann Kravets <kravetsanna78@gmail.com>
2025-07-30 15:35:04 +0300

    add index.html task2.ts

---
+++ Homework/HW2/hw2-1/task2/task2.ts
@@ -0,0 +1,10 @@
+const arr22: string[] = ['Main', 'Products', 'About us', 'Contacts'];
+let menu: HTMLDListElement = document.createElement('ul');
+
+for (const item of arr22) {
+    let li: HTMLLIElement = document.createElement('li');
+    li.innerText = item;
+
+    menu.appendChild(li);
+    document.body.appendChild(menu);
+}
